9b24e7cae7d8f8716e755a1ac33aee12336c19f0
Ireoluwatobi <111185454+Ireoluwatobi@users.noreply.github.com>
2023-07-19 17:18:23 +0100

    bug

--- config/db.js
+++ config/db.js
@@ -1,15 +1,20 @@
-const { connect } = require("mongoose");
+const { connect, default: mongoose } = require("mongoose");
 
 const selectDB = () => {
-  if (process.env.NODE_ENV !== "prodution") return process.env.LOCAL_MONGO_URL;
-      return process.env.MONGO_URL;
+  if (process.env.NODE_ENV == "production"){
+    return process.env.MONGO_URL;
+  }else{
+    return process.env.LOCAL_MONGO_URL;
+  }
+
 };
 
 const createDB = async () => {
   try {
     // mongoose.set("strictQuery", false);
     await connect(selectDB());
-     console.log(`Database connected sucessfully to mongoDB`.green.underline);
+   
+     console.log(`Database connected sucessfully to mongoDB at ${ mongoose.connection.host}`.green.underline);
   } catch (error) {
     console.log(error);
     process.exit(1);
